5c9fba309d59a845b549060335b085b3b7c5da34
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-20 20:54:10 +0200

    Create Commercial.java child of Aircraft.java

---
+++ src/Commercial.java
@@ -0,0 +1,36 @@
+public class Commercial extends Aircraft {
+    private int capacity;
+    private String origin;
+    private String destination;
+
+    public Commercial(String registration, String brand, String model, int crewMembers, int capacity, String origin, String destination) {
+        super(registration, brand, model, crewMembers);
+        this.capacity = capacity;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
