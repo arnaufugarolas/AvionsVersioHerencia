6126c5c46ba2d2bafc4252836a227c235ebbfd27
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-30 23:13:51 +0200

    Reformate file

--- src/Aircraft.java
+++ src/Aircraft.java
@@ -20,30 +20,21 @@ abstract public class Aircraft {
         this.landingGear = true;
     }
 
+    // Getters
     public String getRegistration() { return registration; }
-
     public String getBrand() { return brand; }
-
     public String getModel() { return model; }
-
     public int getCrewMembers() { return crewMembers; }
-
     public Coordinates getCoordinates() { return coordinates; }
-
     public int getAltitude() { return altitude; }
-
     public int getAutonomy() { return autonomy; }
-
     public int getOrientation() { return orientation; }
-
-    public void setCoordinates(Coordinates coordinates) { this.coordinates = coordinates; }
-
-    public boolean isEngine() { return engine; }
-
     public int getSpeed() { return speed; }
-
+    public boolean isEngine() { return engine; }
     public boolean isLandingGear() { return landingGear; }
 
+    // Setters
+    public void setCoordinates(Coordinates coordinates) { this.coordinates = coordinates; }
     public void setCoordinates() {
         int x = f.nextInt("Introdueix la coordenada X: ", "X: ");
         int y = f.nextInt("Introdueix la coordenada Y: ", "Y: ");
@@ -51,14 +42,12 @@ abstract public class Aircraft {
     }
 
     public void setAltitude(int altitude) { this.altitude = altitude; }
-
     public void setAltitude(boolean increase) {
         if (increase) this.altitude = f.nextInt("Altitud: ", this.altitude + 1, Integer.MAX_VALUE);
         else this.altitude = f.nextInt("Altitud: ", 0, this.altitude - 1);
     }
 
     public void setOrientation(int orientation) { this.orientation = orientation; }
-
     public void setOrientation() { this.orientation = f.nextInt("Introdueix el rumb de l'avió", "Rumb: ", 0, 360); }
 
     public void setEngine(boolean engine) {
@@ -72,7 +61,6 @@ abstract public class Aircraft {
     }
 
     public void setSpeed(int speed) { this.speed = speed; }
-
     public void setSpeed(boolean increase) {
         if (increase) this.speed = f.nextInt("Velocitat: ", this.speed + 1, Integer.MAX_VALUE);
         else this.speed = f.nextInt("Velocitat: ", 0, this.speed - 1);
