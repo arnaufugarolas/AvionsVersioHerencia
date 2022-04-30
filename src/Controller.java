6778dce5a159777a4edac4636b26ce563d30900a
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-04-30 21:39:20 +0200

    Update code to the new functions

--- src/Controller.java
+++ src/Controller.java
@@ -6,9 +6,6 @@ public class Controller {
     public static void addAircraft() {
         String registration = null;
         String destination = null;
-        int type = 0;
-        int crewMembers = 0;
-        int autonomy = 0;
         boolean next;
 
         if (airport.getNumberAircraft() >= maximumAircraft) {
@@ -20,12 +17,7 @@ public class Controller {
             return;
         }
 
-        next = false;
-        while (!next) {
-            type = f.nextInt("\nConsola de creació d'avions\nTipus d'avions disponibles:\n\t1- Avió de combat\n\t2- Avió comercial", "Tipus: ");
-            if (type == 1 || type == 2) next = true;
-            else f.printInRed("Tipus d'avió invàlid");
-        }
+        int type = f.nextInt("\nConsola de creació d'avions\nTipus d'avions disponibles:\n\t1- Avió de combat\n\t2- Avió comercial", "Tipus: ", 1 , 2);
 
         next = false;
         while (!next) {
@@ -36,56 +28,18 @@ public class Controller {
 
         String brand = f.nextString("Introdueix la marca de l'avió", "Marca: ");
         String model = f.nextString("Introdueix el model de l'avió", "Model: ");
-
-        next = false;
-        while (!next) {
-            crewMembers = f.nextInt("Introdueix el nombre de tripulants de l'avió", "Tripulants: ");
-            if (crewMembers > 0) next = true;
-            else f.printInRed("Nombre de tripulants invàlid");
-        }
-
-        next = false;
-        while (!next) {
-            autonomy = f.nextInt("Introdueix l'autonomia de l'avió", "Autonomia: ");
-            if (autonomy > 0) next = true;
-            else f.printInRed("Autonomia invàlida");
-        }
+        int crewMembers = f.nextInt("Introdueix el nombre de tripulants de l'avió", "Tripulants: ", 0, Integer.MAX_VALUE);
+        int autonomy = f.nextInt("Introdueix l'autonomia de l'avió", "Autonomia: ", 0, Integer.MAX_VALUE);
 
         if (type == 1) {
-            int missile = 0;
-            int shotDistance = 0;
+            int missile = f.nextInt("Introdueix el nombre de míssils de l'avió", "Míssils: ", 0, Integer.MAX_VALUE);
+            int shotDistance = f.nextInt("Introdueix la distància màxima de llançament de l'avió", "Distància: ", 0, Integer.MAX_VALUE);
 
-            next = false;
-            while (!next) {
-                missile = f.nextInt("Introdueix el nombre de míssils de l'avió", "Míssils: ");
-                if (missile > 0) next = true;
-                else f.printInRed("Nombre de míssils invàlid");
-            }
-
-            next = false;
-            while (!next) {
-                shotDistance = f.nextInt("Introdueix la distància màxima de llançament de l'avió", "Distància: ");
-                if (shotDistance > 0) next = true;
-                else f.printInRed("Distància invàlida");
-            }
-
-            try {
-                airport.addAircraft(new Combat(registration, brand, model, crewMembers, autonomy, missile, shotDistance));
-            }
-            catch (Exception e) {
-                f.printInRed(e.toString());
-            }
+            try { airport.addAircraft(new Combat(registration, brand, model, crewMembers, autonomy, missile, shotDistance)); }
+            catch (Exception e) { f.printInRed(e.toString()); }
         }
         else {
-            int capacity = 0;
-
-            next = false;
-            while (!next) {
-                capacity = f.nextInt("Introdueix la capacitat màxima de l'avió", "Capacitat: ");
-                if (capacity > 0) next = true;
-                else f.printInRed("Capacitat invàlida");
-            }
-
+            int capacity = f.nextInt("Introdueix la capacitat màxima de l'avió", "Capacitat: ", 0, Integer.MAX_VALUE);
             String origin = f.nextString("Introdueix l'origen de l'avió", "Origen: ");
 
             next = false;
@@ -95,12 +49,8 @@ public class Controller {
                 else f.printInRed("El destí de l'avió no pot ser l'origen");
             }
 
-            try {
-                airport.addAircraft(new Commercial(registration, brand, model, crewMembers, autonomy, capacity, origin, destination));
-            }
-            catch (Exception e) {
-                f.printInRed(e.toString());
-            }
+            try { airport.addAircraft(new Commercial(registration, brand, model, crewMembers, autonomy, capacity, origin, destination)); }
+            catch (Exception e) { f.printInRed(e.toString()); }
         }
     }
 
@@ -134,12 +84,11 @@ public class Controller {
                 case 'b' -> aircraft.setEngine(false);
                 case 'c' -> aircraft.setSpeed(-1, true);
                 case 'd' -> aircraft.setSpeed(-1, false);
-                case 'e' -> aircraft.setAltitude(-1, true);
-                case 'f' -> aircraft.setAltitude(-1, false);
+                case 'e' -> aircraft.setAltitude(true);
+                case 'f' -> aircraft.setAltitude(false);
                 case 'g' -> aircraft.setLandingGear(!aircraft.isLandingGear());
                 case 'h' -> aircraft.setOrientation(-1);
-                case 'i' -> f.notImplemented();
-                case 'j' -> f.notImplemented();
+                case 'i', 'j' -> f.notImplemented();
                 case 'k' -> stop = true;
                 default -> f.printInRed("Introdueix una lletra correcta");
             }
@@ -157,12 +106,12 @@ public class Controller {
         airport.addAircraft(new Combat("registration", "brand", "model", 0, 0, 10, 0));
 
         while (!stop) {
-            int number = f.nextInt("\nControlador aeri\n1- Afegir un avió a l’espai aeri.\n2- Gestionar un avió de l’espai Aeri.\n3- Mostrar L’espai Aeri actual.\n4- Sortir", "Opció: ");
+            int number = f.nextInt("\nControlador aeri\n1- Afegir un avió a l’espai aeri.\n2- Gestionar un avió de l’espai Aeri.\n3- Mostrar L’espai Aeri actual.\n4- Sortir", "Opció: ", 1, 4);
             switch (number) {
                 case 1 -> addAircraft();
                 case 2 -> manageAircraft();
                 case 3 -> displayAirSpace();
-                default -> stop = true;
+                case 4 -> stop = true;
             }
         }
     }
